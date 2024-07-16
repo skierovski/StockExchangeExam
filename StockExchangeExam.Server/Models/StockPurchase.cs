8341f6bee65cdab0a87778b4d46283ae8a0f9095
Patrick <skierkowski.patryk.1@gmail.com>
2024-07-16 14:07:13 +0200

    added securing model to implement negative values

--- StockExchangeExam.Server/Models/StockPurchase.cs
+++ StockExchangeExam.Server/Models/StockPurchase.cs
@@ -2,9 +2,33 @@
 {
     public class StockPurchase
     {
+        private int _shares;
+        private decimal _pricePerShare;
+
         public int Id { get; set; }
-        public int Shares { get; set; }
-        public decimal PricePerShare { get; set; }
+
+        public int Shares
+        {
+            get => _shares;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Shares cannot be negative.");
+                _shares = value;
+            }
+        }
+
+        public decimal PricePerShare
+        {
+            get => _pricePerShare;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Price per share cannot be negative.");
+                _pricePerShare = value;
+            }
+        }
+
         public DateTime PurchaseDate { get; set; }
 
     }
