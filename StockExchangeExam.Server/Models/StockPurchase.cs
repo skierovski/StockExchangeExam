577ab6424bf4ef97fb1d58a56d3863a3411d75bd
Patrick <skierkowski.patryk.1@gmail.com>
2024-07-16 08:49:42 +0200

    Created model to hard code what shares are already bought

---
+++ StockExchangeExam.Server/Models/StockPurchase.cs
@@ -0,0 +1,16 @@
+﻿namespace StockExchangeExam.Server.Models
+{
+    public class StockPurchase
+    {
+        public int Id { get; set; }
+        public int Shares { get; set; }
+        public decimal PricePerShare { get; set; }
+
+        public StockPurchase(int id, int shares, decimal pricePerShare)
+        {
+            Id = id;
+            Shares = shares;
+            PricePerShare = pricePerShare;
+        }
+    }
+}
