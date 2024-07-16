65912a95449ce56c0328fef2814c223193f95e3e
Patrick <skierkowski.patryk.1@gmail.com>
2024-07-16 08:58:48 +0200

    Hard coded data of stock already purchased created

--- StockExchangeExam.Server/Controllers/AccountController.cs
+++ StockExchangeExam.Server/Controllers/AccountController.cs
@@ -9,7 +9,18 @@ namespace StockExchangeExam.Server.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-       
+        private readonly List<StockPurchase> _stockPurchases;
+        public AccountController()
+        {
+            _stockPurchases = new List<StockPurchase>
+            {
+                // Here you can change what stock you already purchased i did not create any repository for this because it simpler here to manipulate data
+                new StockPurchase(1, 10, 100),
+                new StockPurchase(2, 20, 200),
+                new StockPurchase(3, 30, 300),
+            };
+        }
+
         [HttpPost]
         [Route("calculate")]
         public IActionResult Calculate([FromBody] SalesRequest salesRequest)
